176c4a7e9af34a3bffdf16744220d5711a3cf448
Blessing <bigbadumhe@gmail.com>
2022-06-06 04:23:26 -0400

    worked on session and logon and logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -43,24 +43,70 @@ router.get("/alljobs", (req, res) => {
     });
 });
 
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "title_body",
+      "budget",
+      "techs",
+      "created_at",
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: User,
+        attributes: ["email"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
 
-router.get('/', (req,res) => {
-  res.render('homepage')
-})
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('login');
+      // pass data to template
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-
-router.get('/signup', (req, res) => {
-  res.render('signup');
+router.get("/", (req, res) => {
+  res.render("homepage");
 });
 
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
 module.exports = router;
