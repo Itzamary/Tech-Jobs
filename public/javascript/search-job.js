f6732421b302c9b781bc7fad427f1d92b8b2991c
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-06-07 13:47:56 -0500

    added a .trim at the end of the techs value to remove any uneeded space and also placed the fetch inside an if statement to make sure the techs variable does have a value before it searches. and also routed the user to a different page if the request was succesfull.

--- public/javascript/search-job.js
+++ public/javascript/search-job.js
@@ -1,25 +1,26 @@
 
 async function searchJob(event) {
   event.preventDefault();
-  const techs = document.querySelector('#jobSearch').value;
+  const techs = document.querySelector('#jobSearch').value.trim();
 
-  console.log(techs);
-
-  const response = await fetch("api/posts/search", {
-    method: "POST",
-    body: JSON.stringify({
+  if (techs) {
+    const response = await fetch("/api/posts/search", {
+      method: "POST",
+      body: JSON.stringify({
       techs,
-    }),
-    headers: {
-      "Content-Type": "application/json"
-    },
-  });
-  if (response.ok) {
-    document.location.replace("/dashboard");
-  } else {
-    console.log(response, 'bad response')
-    alert(response.statusText);
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/alljobs");
+    } else {
+      console.log(response, 'bad response')
+      alert(response.statusText);
+    }
   }
+
+
 }
 
 document.querySelector(".search-button").addEventListener("click", searchJob);
